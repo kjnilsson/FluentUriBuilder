8c07f536c27b6c1a4cf5e6447ebf2a8382bed531
unknown <knilsson@KarlN.adgistics.com>
2012-01-06 20:22:26 +0000

    initial commit

---
+++ FluentUriBuilder.Test/FluentUriBuilderTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FluentUri;
+
+using Xunit;
+
+namespace FluentUriBuilderTest
+{
+    public class FluentUriBuilderTest
+    {
+        [Fact]
+        public void TestNew_ShouldBuildBasicUriFromStringInput()
+        {
+            var result = FluentUriBuilder.New("http://thehost").Build();
+
+            Assert.Equal("thehost", result.Host);
+            Assert.Equal("http", result.Scheme);
+        }
+
+        [Fact]
+        public void TestNew_ShouldBuildUriFromStringInputWithPath()
+        {
+            var result = FluentUriBuilder.New("http://thehost/thepath").Build();
+
+            Assert.Equal("thehost", result.Host);
+            Assert.Equal("http", result.Scheme);
+            Assert.Equal("http://thehost/thepath", result.ToString());
+        }
+
+        [Fact]
+        public void TestNew_ShouldBuildUriFromStringInputWithPathAndQueryString()
+        {
+            var result = FluentUriBuilder.New("http://thehost/thepath?id=54").Build();
+
+            Assert.Equal("thehost", result.Host);
+            Assert.Equal("http", result.Scheme);
+            Assert.Equal("http://thehost/thepath", result.ToString());
+        }
+
+        [Fact]
+        public void TestNew_ShouldBuildUriFromStringInputWithPathAndPortAndQueryString()
+        {
+            var result = FluentUriBuilder.New("http://thehost:8945/thepath?id=54").Build();
+
+            Assert.Equal("thehost", result.Host);
+            Assert.Equal("http", result.Scheme);
+            Assert.Equal("http://thehost:8945/thepath", result.ToString());
+        }
+
+        [Fact]
+        public void TestWithPath_ShouldAddPathToBasicUri()
+        {
+            var result = FluentUriBuilder.New("http://host")
+                .WithPath("mypath/hello")
+                .Build();
+
+            Assert.Equal("/mypath/hello", result.AbsolutePath);
+        }
+
+        [Fact]
+        public void TestWithPath_ShouldAddPathToUriWithPath()
+        {
+            var result = FluentUriBuilder.New("http://host")
+                .WithPath("mypath/hello")
+                .Build();
+
+            Assert.Equal("/mypath/hello", result.AbsolutePath);
+        }
+
+        [Fact]
+        public void TestWithScheme_ShouldChangeScheme()
+        {
+            var result = FluentUriBuilder.New("http://host")
+                .WithScheme("test")
+                .Build();
+
+            Assert.Equal("test", result.Scheme);
+        }
+
+        [Fact]
+        public void TestWithPort_ShouldSetPort()
+        {
+            var result = FluentUriBuilder.New("http://host/path")
+                .WithPort(1234)
+                .Build();
+
+            Assert.Equal(1234, result.Port);
+            Assert.Equal("http://host:1234/path", result.ToString());
+        }
+    }
+}
---
+++ FluentUriBuilder/FluentUriBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentUri
+{
+    public interface IFluentUriBuilder
+    {
+        Uri Build();
+
+        IFluentUriBuilder WithPath(string path);
+
+        IFluentUriBuilder WithScheme(string scheme);
+
+        IFluentUriBuilder WithPort(int port);
+    }
+
+    public class FluentUriBuilder : IFluentUriBuilder
+    {
+        private string uri;
+
+        private string path;
+
+        private string scheme;
+
+        private string host;
+
+        private int port;
+
+        protected FluentUriBuilder(string uri)
+        {
+            var u = new Uri(uri);
+            this.port = u.Port;
+            this.scheme = u.Scheme;
+            this.host = u.Host;
+            this.path = u.AbsolutePath;
+        }
+
+        public static IFluentUriBuilder New(string uri)
+        {
+            return new FluentUriBuilder(uri);
+        }
+
+        public Uri Build()
+        {
+            var builder = new UriBuilder(scheme, host, port, path)
+                {
+                };
+
+            return builder.Uri;
+        }
+
+        public IFluentUriBuilder WithPath(string path)
+        {
+            this.path = path;
+            return this;
+        }
+
+        public IFluentUriBuilder WithScheme(string scheme)
+        {
+            this.scheme = scheme;
+            return this;
+        }
+
+        public IFluentUriBuilder WithPort(int port)
+        {
+            this.port = port;
+            return this;
+        }
+    }
+}
